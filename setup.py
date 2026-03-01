a358684dadb7151dc631c093de520953881c4f41
MidoGhanam's Redmi 9T <mghanam883@gmail.com>
2026-03-01 18:33:34 +0200

    01-03-2026 | 18:33:33

--- setup.py
+++ setup.py
@@ -2,18 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
   name="egydata",
-  version="0.1.0",
-  author="MidoGhanam",
-  author_email="mghanam883@outlook.com",
+  version="1.0.0",
+  author="InnoSoft Company",                # ← change to your company
+  author_email="midoghanam@hotmail.com",        # ← company email
   description="Python library for Egyptian data: cities, governorates, timezones, phone numbers, etc.",
   long_description=open("README.md", encoding="utf-8").read(),
   long_description_content_type="text/markdown",
-  url="https://midoghanam2.pythonanywhere.com/",
+  url="https://www.midoghanam.site/",        # can be your homepage or GitHub
+  project_urls={
+    "Source Code": "https://github.com/InnoSoft-Company/egydata",
+    "Bug Tracker": "https://github.com/InnoSoft-Company/egydata/issues",
+    "Documentation": "https://github.com/InnoSoft-Company/egydata#readme",
+  },
   packages=find_packages(),
   python_requires=">=3.9",
-  install_requires=[
-    # أي مكتبات خارجية تحتاجها
-  ],
+  install_requires=[],
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
